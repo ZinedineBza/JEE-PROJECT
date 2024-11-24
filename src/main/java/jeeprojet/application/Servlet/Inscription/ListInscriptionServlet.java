88bcb1dc4ccc17bcb599798c4492e75cba0437c9
Tytyaz2 <120456383+Tytyaz2@users.noreply.github.com>
2024-11-24 23:36:20 +0100

    merge avec aware

--- src/main/java/jeeprojet/application/Servlet/Inscription/ListInscriptionServlet.java
+++ src/main/java/jeeprojet/application/Servlet/Inscription/ListInscriptionServlet.java
@@ -40,31 +40,24 @@ public class ListInscriptionServlet extends HttpServlet {
         if (Objects.equals(utilisateur.getRole(), "admin")) {
             inscriptions = inscriptionDAO.findAll();
             System.out.println("AAAAAA" + inscriptions);
+            request.setAttribute("inscriptions", inscriptions);
+            request.getRequestDispatcher("/Admin/listInscription.jsp").forward(request, response);
         } else if (Objects.equals(utilisateur.getRole(), "etudiant")) {
             inscriptions = inscriptionDAO.findCoursUtilisateur(utilisateur.getEmail());
             MatiereDAO matiereDAO = new MatiereDAO();
             List<Matiere> matieres = matiereDAO.findAll();
             request.setAttribute("matieres", matieres);
+            request.setAttribute("inscriptions", inscriptions);
+            request.getRequestDispatcher("/Admin/listInscription.jsp").forward(request, response);
         }  else if(Objects.equals(utilisateur.getRole(), "enseignant")) {
             String enseignantEmail = ((Utilisateur) request.getSession().getAttribute("user")).getEmail();
             inscriptions = inscriptionDAO.findByEnseignant(enseignantEmail);
+            request.setAttribute("inscriptions", inscriptions);
+            request.getRequestDispatcher("/Enseignant/listInscriptions.jsp").forward(request, response);
         }else {
             response.sendError(HttpServletResponse.SC_FORBIDDEN, "Accès non autorisé");
             return;
         }
 
-        // Charger explicitement les relations si nécessaire
-        for (Inscription inscription : inscriptions) {
-            if (inscription.getMatiere() != null) {
-                inscription.getMatiere().getNom(); // Charger paresseusement `matiere.nom`
-            }
-            if (inscription.getEtudiant() != null) {
-                inscription.getEtudiant().getEmail(); // Charger paresseusement `etudiant.email`
-            }
-        }
-
-        // Envoyer les données à la JSP
-        request.setAttribute("inscriptions", inscriptions);
-        request.getRequestDispatcher("/Admin/listInscription.jsp").forward(request, response);
     }
 }
